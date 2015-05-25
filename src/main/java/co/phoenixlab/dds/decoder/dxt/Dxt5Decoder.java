2a966aac3014950a5d8efbf27efc57c189fc4559
Vincent Zhang <vzhang@purdue.edu>
2015-05-25 17:29:26 -0400

    FIX: Updated to match super constructor

--- src/main/java/co/phoenixlab/dds/decoder/dxt/Dxt5Decoder.java
+++ src/main/java/co/phoenixlab/dds/decoder/dxt/Dxt5Decoder.java
@@ -5,7 +5,7 @@ import co.phoenixlab.dds.Dds;
 public class Dxt5Decoder extends AbstractDxtDecoder {
 
     public Dxt5Decoder(Dds dds) {
-        super(dds);
+        super(dds, BcHelper.BC3_DXT5_BLOCK_SIZE_BYTES);
     }
 
     @Override
